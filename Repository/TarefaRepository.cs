cc37d940373318f0dae1e5ed3af18888f327c0cf
kauan <melokauan711@gmail.com>
2025-08-03 22:06:52 -0300

    Ajustando status

--- Repository/TarefaRepository.cs
+++ Repository/TarefaRepository.cs
@@ -27,23 +27,16 @@ namespace sistemaDeTarefasT2m.Repository
         public async Task AddTarefaAsync(Tarefas tarefa)
         {
             var query = @"INSERT INTO tarefas
-            (descricao, datacriacao, dataconclusao, userid, status)
-            VALUES(@Descricao, @DataCriacao, @DataConclusao, @UserId,@Status::status_enum)
+            (descricao, datacriacao, dataconclusao, userid)
+            VALUES(@Descricao, @DataCriacao, @DataConclusao, @UserId)
             RETURNING *;";
 
-            // Cria um novo objeto anônimo baseado no DTO, mas com Status convertido em string
-            var tarefaComStatusString = new
-            {
-                tarefa.UserId,
-                tarefa.Descricao,
-                tarefa.DataCriacao,
-                tarefa.DataConclusao,
-                Status = tarefa.Status.ToString()
-            };
+           
+           
 
             using (var connection = _dbConnection.GetConnection())
             {
-                 await connection.QueryAsync<Tarefas>(query, tarefaComStatusString);
+                 await connection.QueryAsync<Tarefas>(query, tarefa);
             }
         }
 
